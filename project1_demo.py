cc18b92ffde0f1898b22b00cd0d65c73848877c7
myrmidon <dada3mn@gmail.com>
2015-04-24 17:29:34 +0200

    some sane project1_demo

--- project1_demo.py
+++ project1_demo.py
@@ -1,6 +1,10 @@
 from src.image_io import import_image
 from src.plot import plot_multiple_arrays
 
-im = import_image("resources/cat.png", as_array=True)
+def main():
+    im = import_image("resources/cat.png", as_array=True)
 
-plot_multiple_arrays([[im]], "Project 1 Demo", ["Input Image"])
+    plot_multiple_arrays([[im]], "Project 1 Demo", ["Input Image"])
+
+if __name__ == "__main__":
+    main()
