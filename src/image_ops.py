5a9f7a1b91a0b7a0f63ee155d2677926cb55bca8
myrmidon <dada3mn@gmail.com>
2015-04-27 10:13:37 +0200

    add TODO

--- src/image_ops.py
+++ src/image_ops.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 def normalize_array(array, new_min=0, new_max=255):
-    assert len(array.shape) == 2, "Only two dimensional arrays supported"
+    assert len(array.shape) == 2, "Only two dimensional arrays supported"  # TODO: @motjuste Why?
     return new_min + \
-            ((array - array.min()) *((new_max - new_min) / (array.max() - array.min())))
+            ((array - array.min()) * ((new_max - new_min) / (array.max() - array.min())))
